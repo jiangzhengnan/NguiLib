9fd0290cef9a7c579024d87220d823e754c9aa68
jiangzhengnan <3442771773@qq.com>
2019-04-27 11:54:06 +0800

    新建Activity

--- app/src/main/java/com/ng/ui/CtbActivity.java
+++ app/src/main/java/com/ng/ui/CtbActivity.java
@@ -9,6 +9,7 @@ public class CtbActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 }
