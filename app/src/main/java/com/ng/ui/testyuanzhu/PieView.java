612abe1b76e34412d833b9d9f4c5b56a5f7033c8
jiangzhengnan <3442771773@qq.com>
2019-12-01 21:56:24 +0800

    add base
    Signed-off-by: jiangzhengnan <3442771773@qq.com>

--- app/src/main/java/com/ng/ui/testyuanzhu/PieView.java
+++ app/src/main/java/com/ng/ui/testyuanzhu/PieView.java
@@ -26,34 +26,34 @@ import androidx.annotation.Nullable;
 public class PieView extends View {
 
     int areaX = 1;
-    int areaY = 22;
+    int areaY = 50;
 
     //饼图的宽高
     private int areaWidth = 360;
     private int areaHight = 300;
 
     //颜色数组
-    private int[] colors =new int[]{
-            Color.rgb(54,217,169),Color.rgb(0,171,255),Color.rgb(80,195,252),Color.rgb(13,142,207),
-            Color.rgb(2,211,21),Color.rgb(176,222,9),Color.rgb(248,255,1),Color.rgb(252,210,2),
-            Color.rgb(255,159,13),Color.rgb(255,100,0),Color.rgb(234,14,0),
+    private int[] colors = new int[]{
+            Color.rgb(54, 217, 169), Color.rgb(0, 171, 255), Color.rgb(80, 195, 252), Color.rgb(13, 142, 207),
+            Color.rgb(2, 211, 21), Color.rgb(176, 222, 9), Color.rgb(248, 255, 1), Color.rgb(252, 210, 2),
+            Color.rgb(255, 159, 13), Color.rgb(255, 100, 0), Color.rgb(234, 14, 0),
     };
     //阴影数组
     private int[] shade_colors = new int[]{
-            Color.rgb(26,164,123),Color.rgb(0,154,230),Color.rgb(21,178,255),Color.rgb(5,102,153),
-            Color.rgb(3,147,15),Color.rgb(124,158,8),Color.rgb(212,218,2),Color.rgb(219,183,6),
-            Color.rgb(214,135,5),Color.rgb(210,90,13),Color.rgb(199,13,1),
+            Color.rgb(26, 164, 123), Color.rgb(0, 154, 230), Color.rgb(21, 178, 255), Color.rgb(5, 102, 153),
+            Color.rgb(3, 147, 15), Color.rgb(124, 158, 8), Color.rgb(212, 218, 2), Color.rgb(219, 183, 6),
+            Color.rgb(214, 135, 5), Color.rgb(210, 90, 13), Color.rgb(199, 13, 1),
     };
 
     //区域百分比
     private int percent[];
 
     private String title;
-    private int thickness= 20;
+
     float x, y;
 
     //默认可响应点击事件
-    private boolean isOntouch= true;
+    private boolean isOntouch = true;
     //下面的图例文字
     private String[] info;
 
@@ -69,11 +69,12 @@ public class PieView extends View {
     private Paint mainPaint;
 
     //默认显示右边
-    private WHERE where  = WHERE.right;
+    private WHERE where = WHERE.right;
 
-    public static enum WHERE{
-        right ,bottmo
+    public static enum WHERE {
+        right, bottmo
     }
+
     public PieView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
@@ -101,8 +102,8 @@ public class PieView extends View {
 //    }
 
     //初始化 画笔
-    private void initPaint(){
-        this.percent = new int[]{60, 120, 30, 150};
+    private void initPaint() {
+        this.percent = new int[]{30, 120, 30, 180};
 
         this.info = new String[]{"a", "a", "a", "a"};
         legendPaint = new Paint();
@@ -115,23 +116,25 @@ public class PieView extends View {
     }
 
     //设置饼图的大小  这里已经给了默认大小了
-    public void  setSize(int areaWidth ,int areaHight){
+    public void setSize(int areaWidth, int areaHight) {
         this.areaWidth = areaWidth;
         this.areaHight = areaHight;
     }
 
     //饼图的标题
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setWhere(WHERE s){
-        this.where =  s;
+    public void setWhere(WHERE s) {
+        this.where = s;
     }
 
+    //下面得阴影厚度
+    private int thickness = 50;
     public void setThickness(int thickness) {
         this.thickness = thickness;
-        areaY=thickness+2;
+        areaY = thickness + 2;
         this.invalidate();
     }
 
@@ -143,41 +146,40 @@ public class PieView extends View {
         super.onDraw(canvas);
 
         //开画
-        for(int i=0;i <= thickness;i++){
+        for (int i = 0; i <= thickness; i++) {
 
-            int tempAngle=0;
-            for(int j=0;j< percent.length;j++){
+            int tempAngle = 0;
+            for (int j = 0; j < percent.length; j++) {
+
+
                 //画笔的颜色
                 mainPaint.setColor(shade_colors[j]);
                 //坐标点 start end
-                RectF rectF = new RectF(areaX, areaY-i, areaX+areaWidth, areaHight-i);
+                RectF rectF = new RectF(areaX, areaY - i, areaX + areaWidth, areaHight - i);
                 //画弧
-                canvas.drawArc(rectF, tempAngle,percent[j], true, mainPaint);
-                tempAngle+=percent[j];
+                canvas.drawArc(rectF, tempAngle, percent[j], true, mainPaint);
+                tempAngle += percent[j];
             }
 
 
             //开始画图例区域
-            if(i  == thickness ){
+            if (i == thickness) {
 
-                RectF rectF = new RectF(areaX, areaY- thickness, areaX + areaWidth, areaHight- thickness);
+                RectF rectF = new RectF(areaX, areaY - thickness, areaX + areaWidth, areaHight - thickness);
                 centerX = rectF.centerX();
                 centerY = rectF.centerY();
 
                 int temp = areaHight + 20/* height-320*/;
 
-                for(int j=0;j<percent.length;j++){
-
-
+                for (int j = 0; j < percent.length; j++) {
                     mainPaint.setColor(colors[j]);
-                    canvas.drawArc(rectF , tempAngle,percent[j], true, mainPaint);
-                    tempAngle+=percent[j];
-
+               //  canvas.drawArc(rectF , tempAngle,percent[j], true, mainPaint);
+                    tempAngle += percent[j];
 
                     //说明区域
-                    RectF rect = new RectF(areaX, temp, areaX+40, temp-10);//标识区域
-                    canvas.drawText(info[j], areaX+60, temp, legendPaint);
-                    canvas.drawRect(rect, mainPaint);
+                    RectF rect = new RectF(areaX, temp, areaX + 40, temp - 10);//标识区域
+//                    canvas.drawText(info[j], areaX + 60, temp, legendPaint);
+//                    canvas.drawRect(rect, mainPaint);
                     temp += 25;
                 }
             }
@@ -188,9 +190,9 @@ public class PieView extends View {
 
             if (isOntouch) {
                 isOntouch = false;
-                System.out.println(centerX + ""  + centerY);
+                System.out.println(centerX + "" + centerY);
 
-                double d = Math.atan2(y-centerY, x-centerX)*180/Math.atan2(0.0, -1.0);
+                double d = Math.atan2(y - centerY, x - centerX) * 180 / Math.atan2(0.0, -1.0);
 
                 if (d < 0) {
                     d = 360 + d;
@@ -201,14 +203,14 @@ public class PieView extends View {
 
                     Log.e(Float.toString(y), Float.toString(areaHight));
 
-                    if (d >temp && d < percent[j]+temp && y < areaHight) {
+                    if (d > temp && d < percent[j] + temp && y < areaHight) {
                         System.out.println(" " + colors[j]);
 
 //						context.startActivity(it);
 
-                        Toast.makeText(context, "colors[j]="+ colors[j]
-                                        + "----x,y="+ x+","+y
-                                        +"--temp="+ temp,
+                        Toast.makeText(context, "colors[j]=" + colors[j]
+                                        + "----x,y=" + x + "," + y
+                                        + "--temp=" + temp,
                                 1000).show();
                     }
                     temp += percent[j];
--- nguilib/src/main/java/com/ng/nguilib/CylinderView.java
+++ nguilib/src/main/java/com/ng/nguilib/CylinderView.java
@@ -1,17 +1,12 @@
 package com.ng.nguilib;
 
-import android.animation.PropertyValuesHolder;
-import android.animation.TimeInterpolator;
-import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -24,175 +19,97 @@ import androidx.annotation.Nullable;
  * @CreateDate: 2019/11/24
  */
 public class CylinderView extends View {
-    private Paint mWhitePaint;
-    private Paint mCirclePaint;
-    private float mCircleStrokeWidth = 2;
-    private float mMaxRadius = 200;
-
-    /* Camera旋转的最大角度 */
-    private float mMaxCameraRotate = 15;
-
-    /* 我们今天的主角 */
-    private Matrix mMatrix;
-    private Camera mCamera;
-
-    /* Camera绕X轴旋转的角度 */
-    private float mCameraRotateX;
-    /* Camera绕Y轴旋转的角度 */
-    private float mCameraRotateY;
-
-    /* 手指松开时时钟晃动的动画 */
-    private ValueAnimator mShakeAnim;
 
 
-    private void init(){
-        mMatrix = new Matrix();
-        mCamera = new Camera();
+    //区域百分比
+    private int percent[];
 
-        //白色大圆的画笔
-        mWhitePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mWhitePaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        mWhitePaint.setStrokeWidth(mCircleStrokeWidth);
-        mWhitePaint.setColor(Color.WHITE);
+    //饼图的paint
+    private Paint mainPaint;
+    //饼图的宽高
+    private int areaWidth = 360;
+    private int areaHight = 450;
 
-        //内部蓝色圆环的画笔
-        mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mCirclePaint.setStyle(Paint.Style.STROKE);
-        mCirclePaint.setStrokeWidth(mCircleStrokeWidth);
-        mCirclePaint.setColor(Color.WHITE);
-        mCirclePaint.setColor(0xff237EAD);
-    }
+    //点击事件的计算
+    private float centerX;
+    private float centerY;
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        setCameraRotate(canvas);
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, mMaxRadius, mWhitePaint);
+    //颜色数组
+    private int[] colors = new int[]{
+            Color.rgb(54, 217, 169), Color.rgb(0, 171, 255), Color.rgb(80, 195, 252), Color.rgb(13, 142, 207),
+            Color.rgb(2, 211, 21), Color.rgb(176, 222, 9), Color.rgb(248, 255, 1), Color.rgb(252, 210, 2),
+            Color.rgb(255, 159, 13), Color.rgb(255, 100, 0), Color.rgb(234, 14, 0),
+    };
+    //阴影数组
+    private int[] shade_colors = new int[]{
+            Color.rgb(26, 164, 123), Color.rgb(0, 154, 230), Color.rgb(21, 178, 255), Color.rgb(5, 102, 153),
+            Color.rgb(3, 147, 15), Color.rgb(124, 158, 8), Color.rgb(212, 218, 2), Color.rgb(219, 183, 6),
+            Color.rgb(214, 135, 5), Color.rgb(210, 90, 13), Color.rgb(199, 13, 1),
+    };
 
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, mMaxRadius / 6 * 2, mCirclePaint);
-    }
-
-    private void setCameraRotate(Canvas mCanvas) {
-        mMatrix.reset();
-        mCamera.save();
-        mCamera.rotateX(mCameraRotateX);//绕x轴旋转
-        mCamera.rotateY(mCameraRotateY);//绕y轴旋转
-        mCamera.getMatrix(mMatrix);//计算对于当前变换的矩阵，并将其复制到传入的mMatrix中
-        mCamera.restore();
-        /**
-         * Camera默认位于视图的左上角，故生成的矩阵默认也是以其左上角为旋转中心，
-         * 所以在动作之前调用preTranslate将mMatrix向左移动getWidth()/2个长度，
-         * 向上移动getHeight()/2个长度，
-         * 使旋转中心位于矩阵的中心位置，动作之后再post回到原位
-         */
-        mMatrix.preTranslate(-getWidth() / 2, -getHeight() / 2);
-        mMatrix.postTranslate(getWidth() / 2, getHeight() / 2);
-        mCanvas.concat(mMatrix);//将mMatrix与canvas中当前的Matrix相关联
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                if (mShakeAnim != null && mShakeAnim.isRunning()) {
-                    mShakeAnim.cancel();
-                }
-                getCameraRotate(event);
-                invalidate();
-            case MotionEvent.ACTION_MOVE:
-                //根据手指坐标计算Camera应该旋转的角度
-                getCameraRotate(event);
-                invalidate();
-                break;
-            case MotionEvent.ACTION_UP:
-                //松开手指，时钟复原并伴随晃动动画
-                startShakeAnim();
-                break;
-        }
-        return true;
-    }
-    /**
-     * 时钟晃动动画
-     */
-    private void startShakeAnim() {
-        Log.d("nangua","staertDONGHUA");
-        final String cameraRotateXName = "cameraRotateX";
-        final String cameraRotateYName = "cameraRotateY";
-        PropertyValuesHolder cameraRotateXHolder =
-                PropertyValuesHolder.ofFloat(cameraRotateXName, mCameraRotateX, 0);
-        PropertyValuesHolder cameraRotateYHolder =
-                PropertyValuesHolder.ofFloat(cameraRotateYName, mCameraRotateY, 0);
-        mShakeAnim = ValueAnimator.ofPropertyValuesHolder(cameraRotateXHolder,
-                cameraRotateYHolder );
-        mShakeAnim.setInterpolator(new TimeInterpolator() {
-            @Override
-            public float getInterpolation(float input) {
-                //http://inloop.github.io/interpolator/
-                float f = 0.571429f;
-                return (float) (Math.pow(2, -2 * input) * Math.sin((input - f / 4) * (2 * Math.PI) / f) + 1);
-            }
-        });
-        mShakeAnim.setDuration(500);
-        mShakeAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                mCameraRotateX = (float) animation.getAnimatedValue(cameraRotateXName);
-                mCameraRotateY = (float) animation.getAnimatedValue(cameraRotateYName);
-                postInvalidate();
-            }
-        });
-        mShakeAnim.start();
-    }
-
-
-    private void getCameraRotate(MotionEvent event) {
-        float rotateX = -(event.getY() - getHeight() / 2);
-        float rotateY = (event.getX() - getWidth() / 2);
-        /**
-         *为什么旋转角度要这样计算：
-         * 当Camera.rotateX(x)的x为正时，图像围绕X轴，上半部分向里下半部分向外，进行旋转，
-         * 也就是手指触摸点要往上移。这个x就会与event.getY()的值有关，x越大，绕X轴旋转角度越大，
-         * 以圆心为基准，手指往上移动，event.getY() - getHeight() / 2的值为负，
-         * 故 float rotateX = -(event.getY() - getHeight() / 2)
-         * 同理，
-         * 当Camera.rotateY(y)的y为正时，图像围绕Y轴，右半部分向里左半部分向外，进行旋转，
-         * 也就是手指触摸点要往右移。这个y就会与event.getX()的值有关，y越大，绕Y轴旋转角度越大，
-         * 以圆心为基准，手指往右移动，event.getX() - getWidth() / 2的值为正，
-         * 故 float rotateY = event.getX() - getWidth() / 2
-         */
-
-        /**
-         * 此时得到的rotateX、rotateY 其实是以圆心为基准，手指移动的距离，
-         * 这个值很大，不能用来作为旋转的角度，
-         * 所以还需要继续处理
-         */
-
-        //求出移动距离与半径之比。mMaxRadius为白色大圆的半径
-        float percentX = rotateX / mMaxRadius;
-        float percentY = rotateY / mMaxRadius;
-
-        if (percentX > 1) {
-            percentX = 1;
-        } else if (percentX < -1) {
-            percentX = -1;
-        }
-
-        if (percentY > 1) {
-            percentY = 1;
-        } else if (percentY < -1) {
-            percentY = -1;
-        }
-
-        //将最终的旋转角度控制在一定的范围内，这里mMaxCameraRotate的值为15，效果比较好
-        mCameraRotateX = percentX * mMaxCameraRotate;
-        mCameraRotateY = percentY * mMaxCameraRotate;
-    }
     public CylinderView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        initPaint();
     }
 
     public CylinderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        initPaint();
+    }
+
+    //初始化 画笔
+    private void initPaint() {
+        this.percent = new int[]{30, 120, 180, 30};
+
+        mainPaint = new Paint();
+        mainPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mainPaint.setAntiAlias(true);
+    }
+
+    int areaX = 1;
+    int areaY = 200;
+    private int thickness = 30;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //开画
+        int tempAngle = 0;
+        for (int j = 0; j < percent.length; j++) {
+            mainPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+            //画笔的颜色
+            mainPaint.setColor(shade_colors[j]);
+
+
+            //坐标点 start end
+            RectF rectF = new RectF(areaX, areaY - 1, areaX + areaWidth, areaHight - 1);
+            if (j == percent.length-1) {
+                for (int i = 0; i <= thickness; i++) {
+                    rectF = new RectF(areaX, areaY -  i, areaX + areaWidth, areaHight -   i);
+                    Log.d("nangua", "俺几次");
+
+                    if (i == thickness) {
+                        mainPaint.setStyle(Paint.Style.STROKE);
+                        mainPaint.setColor( Color.rgb(255, 255, 255));
+
+                    }
+
+                    canvas.drawArc(rectF, tempAngle, percent[j], true, mainPaint);
+
+                }
+
+            } else {
+                Log.d("nangua", "别的几次");
+                rectF = new RectF(areaX, areaY , areaX + areaWidth, areaHight  );
+
+                //画弧
+                 canvas.drawArc(rectF, tempAngle, percent[j], true, mainPaint);
+
+            }
+            tempAngle += percent[j];
+        }
+
+
     }
 }
