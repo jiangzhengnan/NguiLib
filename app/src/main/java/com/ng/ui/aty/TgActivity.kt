72ff223844eb7b0f9a1ca0d542ecca2c70534ed5
jzn <jzn@webull.com>
2020-02-28 14:47:11 +0800

    init Toggle view

---
+++ app/src/main/java/com/ng/ui/aty/TgActivity.kt
@@ -0,0 +1,12 @@
+package com.ng.ui.aty
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 描述:
+ * @author Jzn
+ * @date 2020-02-28
+ */
+class TgActivity : AppCompatActivity() {
+
+}
---
+++ nguilib/src/main/java/com/ng/nguilib/ToggleView.java
@@ -0,0 +1,23 @@
+package com.ng.nguilib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 描述:
+ *
+ * @author Jzn
+ * @date 2020-02-28
+ */
+public class ToggleView extends View {
+    public ToggleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ToggleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
