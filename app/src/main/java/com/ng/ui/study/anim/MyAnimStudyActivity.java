4d3b81e07c23244a4354e50ddfb7d87e9daa681f
jiangzhengnan <3442771773@qq.com>
2020-05-13 10:35:49 +0800

    add

--- app/src/main/java/com/ng/ui/study/anim/MyAnimStudyActivity.java
+++ app/src/main/java/com/ng/ui/study/anim/MyAnimStudyActivity.java
@@ -5,10 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.ng.nguilib.utils.LogUtils;
 import com.ng.ui.R;
-
-import java.util.List;
 
 /**
  * 描述:
@@ -17,26 +14,30 @@ import java.util.List;
  * @date 2020-04-24
  */
 public class MyAnimStudyActivity extends AppCompatActivity {
-    private PieAnimView pie_test;
+    private PieAnimView pie_test1;
+    private PieAnimView pie_test2;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anim_study);
-        pie_test = findViewById(R.id.pie_test);
+        pie_test1 = findViewById(R.id.pie_test1);
+        pie_test2 = findViewById(R.id.pie_test2);
 
-        List<PieAnimShape> pieAnimShapes = PieResolver.getInstance().readShapesFromAssert(this, "check_cross.json");
-        LogUtils.INSTANCE.d("result:" + pieAnimShapes.toString());
 
-        pie_test.setAnimDate(pieAnimShapes);
+        pie_test1.setAnimDate(PieResolver.getInstance().readShapesFromAssert(this, "test.json"));
+        pie_test1.post(() -> pie_test1.showShape());
 
-        pie_test.post(() -> pie_test.showShape());
+        pie_test2.setAnimDate(PieResolver.getInstance().readShapesFromAssert(this, "test1.json"));
+        pie_test2.post(() -> pie_test2.showShape());
 
         findViewById(R.id.btn_test).setOnClickListener(v -> startTest());
     }
 
     private void startTest() {
-        pie_test.changeShape();
+        pie_test1.changeShape();
+        pie_test2.changeShape();
     }
 
 
