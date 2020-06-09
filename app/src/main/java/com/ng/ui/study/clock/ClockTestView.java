8b9fbbb510d6386bef13287317d56e01719f3bed
jiangzhengnan <3442771773@qq.com>
2020-06-09 09:42:08 +0800

    add

---
+++ app/src/main/java/com/ng/ui/study/clock/ClockTestView.java
@@ -0,0 +1,21 @@
+package com.ng.ui.study.clock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 描述:
+ *
+ * @author Jzn
+ * @date 2020-06-05
+ */
+public class ClockTestView extends View {
+
+
+    public ClockTestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
