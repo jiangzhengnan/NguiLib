6f22f3fecd7df8507cef8ed60a5dcea2becf6f12
jiangzhengnan <3442771773@qq.com>
2020-10-27 20:31:19 +0800

    add

---
+++ app/src/main/java/com/ng/ui/test/ListNode.java
@@ -0,0 +1,16 @@
+package com.ng.ui.test;
+
+/**
+ * 描述:
+ *
+ * @author Jzn
+ * @date 2020/10/27
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
--- app/src/main/java/com/ng/ui/test/TestClass.java
+++ app/src/main/java/com/ng/ui/test/TestClass.java
@@ -1,57 +1,66 @@
 package com.ng.ui.test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /*
-https://leetcode-cn.com/problems/find-common-characters/
-给定仅有小写字母组成的字符串数组 A，返回列表中的每个字符串中都显示的全部字符（包括重复字符）组成的列表。
-例如，如果一个字符在每个字符串中出现 3 次，但不是 4 次，则需要在最终答案中包含该字符 3 次。
-你可以按任意顺序返回答案。
-示例 1：
-输入：["bella","label","roller"]
-输出：["e","l","l"]
+学校在拍年度纪念照时，一般要求学生按照 非递减 的高度顺序排列。
+
+请你返回能让所有学生以 非递减 高度排列的最小必要移动人数。
+
+注意，当一组学生被选中时，他们之间可以以任何可能的方式重新排序，而未被选中的学生应该保持不动。
+
+ 
+
+示例：
+
+输入：heights = [1,1,4,2,1,3]
+输出：3
+解释：
+当前数组：[1,1,4,2,1,3]
+目标数组：[1,1,1,2,3,4]
+在下标 2 处（从 0 开始计数）出现 4 vs 1 ，所以我们必须移动这名学生。
+在下标 4 处（从 0 开始计数）出现 1 vs 3 ，所以我们必须移动这名学生。
+在下标 5 处（从 0 开始计数）出现 3 vs 4 ，所以我们必须移动这名学生。
 示例 2：
-输入：["cool","lock","cook"]
-输出：["c","o"]
+
+输入：heights = [5,1,2,3,4]
+输出：5
+示例 3：
+
+输入：heights = [1,2,3,4,5]
+输出：0
+ 
 
 提示：
-1 <= A.length <= 100
-1 <= A[i].length <= 100
-A[i][j] 是小写字母
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
  */
 public class TestClass {
 
     public static void main(String[] args) {
-        commonChars(new String[]{"bella", "label", "roller"});
+        LogUtils.print(heightChecker(new int[]{1,2,3,4,5})+"");
     }
 
-    public static List<String> commonChars(String[] A) {
-        int[] fir = new int[26];
-        Arrays.fill(fir, Integer.MAX_VALUE);
-
-        for (String temp : A) {
-            int[] tempfir = new int[26];
-            for (char charTemp : temp.toCharArray()) {
-                tempfir[charTemp - 'a']++;
-            }
-
-            //比较
-            for (int i = 0; i < 26; i++) {
-                fir[i] = Math.min(fir[i], tempfir[i]);
+    public static int heightChecker(int[] heights) {
+        int result = 0;
+        int[] nums = new int[100];
+        for (int temp : heights) {
+            nums[temp]++;
+        }
+        int[] sortHeights = new int[heights.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i]; j++) {
+                sortHeights[index] = i;
+                index++;
             }
         }
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < fir[i]; j++) {
-                char temp = (char) ('a' + i);
-                result.add(String.valueOf(temp));
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != sortHeights[i]) {
+                result++;
             }
         }
-        LogUtils.print(result.toString());
+
         return result;
     }
-
-
 }
