d04fea853106d613957b6e3bd20e3f8072b07c6e
Jzn <3442771773@qq.com>
2021-01-12 15:06:41 +0800

    add

---
+++ app/src/main/java/com/ng/ui/other/dragdialog/DragDialogLayout.kt
@@ -0,0 +1,25 @@
+package com.ng.ui.other.dragdialog
+
+import android.content.Context
+import android.view.View
+
+/**
+ * 描述:
+ * @author Jzn
+ * @date 2021/1/12
+ */
+class DragDialogLayout(context: Context?) : View(context) {
+
+    init {
+
+
+    }
+
+    fun show(){
+
+    }
+
+    fun disMiss(){
+
+    }
+}
--- app/src/main/java/com/ng/ui/test/TestActivityK.kt
+++ app/src/main/java/com/ng/ui/test/TestActivityK.kt
@@ -2,6 +2,9 @@ package com.ng.ui.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ng.ui.R
+import com.ng.ui.other.dragdialog.DragDialogLayout
+import kotlinx.android.synthetic.main.activity_test.*
 
 /**
  * 描述:
@@ -12,7 +15,9 @@ class TestActivityK : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setContentView(R.layout.activity_test)
+        btn_add.setOnClickListener {
+            DragDialogLayout(this).show()
+        }
     }
 }
